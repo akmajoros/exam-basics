2c8674b09a875b97aa2250efae67f64f7f8a8412
Kristof Majoros <akmajoros1@gmail.com>
2017-04-18 13:23:39 +0200

    sorry, this is the last one, just renamed some stuff in Copy

--- exam-tasks/src/Copy.java
+++ exam-tasks/src/Copy.java
@@ -16,11 +16,11 @@ public class Copy {
 
   public void printFiletoOther(String[] args) {
     Path filePath = Paths.get(args[0]);
-    Path newPath = Paths.get(args[1]);
+    Path pathToCopy = Paths.get(args[1]);
     try {
       List<String> content = Files.readAllLines(filePath);
       List<String> toCopy = new ArrayList<>();
-        Files.write(newPath, content);
+        Files.write(pathToCopy, content);
         System.out.println("Copy successfully performed");
     } catch (Exception e){
       System.out.println("I'm sorry, I am unable to read the file");
